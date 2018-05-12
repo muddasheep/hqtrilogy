4479d083d4623b927f5046d0be5280d810e39f5c
muddasheep <muddasheep@gmail.com>
2018-05-12 21:40:27 +0200

    prepare full build

--- compile/copymap.bat
+++ compile/copymap.bat
@@ -1,6 +1,6 @@
 @echo off
 copy \programme\hammer\compile\%1.bsp "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\hqtrilogy\maps\"%1.bsp
-copy \programme\hammer\compile\%1.bsp "C:\Users\muddasheep\Documents\hqtrilogy\hqtrilogy\maps\"%1.bsp
+copy \programme\hammer\compile\%1.bsp "C:\Users\muddasheep\Documents\hqtrilogy\build\hqtrilogy\maps\"%1.bsp
 copy \programme\hammer\maps\hqtrilogy\%1.rmf "C:\Users\muddasheep\Documents\hqtrilogy\maps\"%1.rmf
 echo %1 copied.
 echo.
--- compile/test.bat
+++ compile/test.bat
@@ -1,5 +1,9 @@
 @echo off
 copy "C:\Users\muddasheep\Documents\hqtrilogy\SpiritSource18\dlls\Releasehl\spirit.dll" "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\hqtrilogy\dlls\spirit.dll"
 copy "C:\Users\muddasheep\Documents\hqtrilogy\SpiritSource18\cl_dll\Release\client.dll" "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\hqtrilogy\cl_dlls\client.dll"
+
+copy "C:\Users\muddasheep\Documents\hqtrilogy\SpiritSource18\dlls\Releasehl\spirit.dll" "C:\Users\muddasheep\Documents\hqtrilogy\build\hqtrilogy\dlls\spirit.dll"
+copy "C:\Users\muddasheep\Documents\hqtrilogy\SpiritSource18\cl_dll\Release\client.dll" "C:\Users\muddasheep\Documents\hqtrilogy\build\hqtrilogy\cl_dlls\client.dll"
+
 echo Starting %1.bsp ...
 "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\hl.exe" -console -game hqtrilogy -dev +map %1
